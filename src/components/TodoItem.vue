b76031b7ebc71124ed78a19db79e0dde30c21bed
atidivya <atidivya@gmail.com>
2020-12-15 12:05:57 +0000

    update styles

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="todo-item" @click="importantTodo(todo.id)">
-        <div class="user-profile_todo">
-            <div class="todo-item_user">
-                {{username}}    
-            </div> 
-            <div class="todo-item_content">
-                {{todo.content}}
-            </div>
-        </div>
+  <div class="todo-item" @click="importantTodo(todo.id)">
+    <div class="user-profile_todo">
+      <div class="todo-item_user">
+        {{username}}
+      </div>
+      <div class="todo-item_content">
+        {{todo.content}}
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -32,23 +32,26 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
 .todo-item {
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #DFE3E8;
-    box-sizing: border-box;
-    cursor: pointer;
-    transition: all 0.25s ease;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.25s ease;
+
+  &:hover {
+  transform: scale(1.1, 1.1);
 }
 
-.todo-item:hover {
-    transform: scale(1.1, 1.1);
+  .todo-item_user { 
+  font-weight: bold;
+}
 }
 
-.todo-item_user {
-    font-weight: bold;
-}
+
+
+
 </style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,29 +1,45 @@
 <template>
   <div class="user-profile">
-      <div class="user-profile_user-panel">
-          <h1 class="user-profile_username">{{user.username}}</h1>
-          <div class="user-profile_admin-badge" v-if="user.isAdmin">Admin</div>
-          <div class="user-profile_follower-count">
-                <strong>Followers: </strong> {{followers}}
-          </div>
-          <form class="user-profile_create-todo" @submit.prevent="createNewTodo">
-              <label for="newTodo"><strong>New Todo</strong></label>
-              <textarea name="" id="newTodo" cols="30" rows="4" v-model="newTodoContent"/>
+    <div class="user-profile_user-panel">
+      <h1 class="user-profile_username">{{user.username}}</h1>
+      <div class="user-profile_admin-badge" v-if="user.isAdmin">Admin</div>
+      <div class="user-profile_follower-count">
+        <strong>Followers: </strong> {{followers}}
+      </div>
+      <form class="user-profile_create-todo" @submit.prevent="createNewTodo" :class="{ '--exceeded': newTodoCharacterCount > 180}">
+        <label for="newTodo"><strong>New Todo</strong>({{ newTodoCharacterCount}}/180)</label>
+        <textarea
+          name=""
+          id="newTodo"
+          cols="30"
+          rows="4"
+          v-model="newTodoContent"
+        />
 
-              <div class="user-profile_create-todo-type">
-                  <label for="newTodoType"><strong>Type: </strong></label>
-                  <select id="newTodoType" v-model="selectedTodoType">
-                      <option :value="option.value" v-for="(option, index) in todoTypes" :key="index"> {{option.name}}</option>
-                  </select>
-              </div>
-              <button>
-                  Add Todo!
-              </button>
-          </form> 
-      </div>
-      <div class="user-profile_todos-wrapper">
-          <TodoItem v-for="todo in user.todos" :key="todo.id" :username="user.username" :todo="todo" @important="toggleImportant" />
-      </div>
+        <div class="user-profile_create-todo-type">
+          <label for="newTodoType"><strong>Type: </strong></label>
+          <select id="newTodoType" v-model="selectedTodoType">
+            <option
+              :value="option.value"
+              v-for="(option, index) in todoTypes"
+              :key="index"
+            >
+              {{option.name}}
+            </option>
+          </select>
+        </div>
+        <button>Add Todo</button>
+      </form>
+    </div>
+    <div class="user-profile_todos-wrapper">
+      <TodoItem
+        v-for="todo in user.todos"
+        :key="todo.id"
+        :username="user.username"
+        :todo="todo"
+        @important="toggleImportant"
+      />
+    </div>
   </div>
 </template>
 
@@ -64,8 +80,8 @@ export default {
     }
   },
   computed: {
-    fullName(){
-      return `${this.user.firstName} ${this.user.lastName} `;
+    newTodoCharacterCount() {
+      return this.newTodoContent.length;
     }
   },
   methods:{
@@ -91,45 +107,49 @@ export default {
 }
 </script>
 
-<style>
-   .user-profile {
-       display: grid;
-       grid-template-columns: 1fr 3fr;
-       width: 100%;
-       padding: 50px 5%;
-   }
+<style lang="scss" scoped>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
 
-   .user-profile_user-panel {
-       display: flex;
-       flex-direction: column;
-       margin-right: 50px;
-       padding: 20px;
-       background-color: white;
-       border-radius: 5px;
-       border: 1px solid #dfe3e8;
-   }
+  .user-profile_user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #dfe3e8;
 
-   .user-profile_admin-badge {
-       background: rebeccapurple;
-       color: white;
-       border-radius: 5px;
-       margin-right: auto;
-       padding: 0 10px;
-       font-weight: bold;
-   }
+    h1 {
+      margin: 0;
+    }
+    .user-profile_admin-badge {
+      background: rebeccapurple;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      padding: 0 10px;
+      font-weight: bold;
+    }
 
-   h1{
-       margin: 0;
-   }
+    .user-profile_create-todo {
+      padding-top: 20px;
+      display: flex;
+      flex-direction: column;
 
-   .user-profile_todos-wrapper {
-       display: grid;
-       grid-gap: 10px;
-   }
+      &.--exceeded {
+        color: red;
+        border-color:none;
+      }
+    }
+  }
 
-   .user-profile_create-todo {
-       padding-top: 20px;
-       display: flex;
-       flex-direction: column;
-   }
+  .user-profile_todos-wrapper {
+    display: grid;
+    grid-gap: 10px;
+  }
+}
 </style>
